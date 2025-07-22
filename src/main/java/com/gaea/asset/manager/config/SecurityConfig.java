540e458c50a29150528356acd17364056e9abc56
hslee134 <hslee134@example.com>
2025-07-22 16:08:30 +0900

    [KPI] Spring Security 설정, JWT Filter 추가

---
+++ src/main/java/com/gaea/asset/manager/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.gaea.asset.manager.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.gaea.asset.manager.filter.CustomUserDetailsService;
+import com.gaea.asset.manager.filter.JwtAuthenticationFilter;
+import com.gaea.asset.manager.util.JwtUtil;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;;
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter(jwtUtil, customUserDetailsService);
+	}
+
+	private static final String[] AUTH_WHITELIST = {
+		"/auth/**", "/swagger-ui/**", "/v3/api-docs/**"
+	};
+	
+	@SuppressWarnings("removal")
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors(Customizer.withDefaults())
+			.csrf().disable()
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(AUTH_WHITELIST).permitAll()
+				.anyRequest().authenticated()
+			)
+			.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:3000"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);  // JWT 쿠키 또는 인증정보 전송 허용
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+}
--- src/main/java/com/gaea/asset/manager/config/SwaggerConfig.java
+++ src/main/java/com/gaea/asset/manager/config/SwaggerConfig.java
@@ -1,25 +1,30 @@
 package com.gaea.asset.manager.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
 	@Bean
 	public OpenAPI openAPI() {
 		return new OpenAPI()
-				.components(new Components())
-				.info(apiInfo());
-	}
-
-	private Info apiInfo() {
-		return new Info()
-				.title("Gaea Asset API") // API의 제목
-				.description("Gaea Asset API Swagger UI") // API에 대한 설명
-				.version("0.1"); // API의 버전
+				.info(new Info().title("Gaea Asset API").description("Gaea Asset API Swagger UI").version("v0.1"))
+				.components(new Components()
+						.addSecuritySchemes("bearerAuth", new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
 	}
 }
