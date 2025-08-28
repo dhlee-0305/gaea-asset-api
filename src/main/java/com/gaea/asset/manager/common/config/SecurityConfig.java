2b2b47c0d89a9006557c0764bc74e41be7b0b913
hslee134 <hslee134@example.com>
2025-08-28 14:51:44 +0900

    설정 추가

--- src/main/java/com/gaea/asset/manager/common/config/SecurityConfig.java
+++ src/main/java/com/gaea/asset/manager/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,6 +43,7 @@ public class SecurityConfig {
 		http.cors(Customizer.withDefaults())
 			.csrf().disable()
 			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.requestMatchers(AUTH_WHITELIST).permitAll()
 				.anyRequest().authenticated()
 			)
